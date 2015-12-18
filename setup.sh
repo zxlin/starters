dbf96bb51615800baaf75423ebeecbbe22c80ee6
Zhi Xiang Lin <zhixiang94@gmail.com>
2015-12-18 16:59:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cp -r ./vim/* ~/
-cp -r ./git/* ~/
-cp -r ./bash/* ~/
+cp -r ./vim/.vimrc ~/
+cp -r ./vim/.vim/ ~/.vim/
+cp -r ./git/.gitconfig ~/.git/
+cat ./bash/.bash_profile >> ~/.bash_profile
