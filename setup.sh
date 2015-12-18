b850a7a9f32d2ec6d98e9c025299d512f7fc1f9f
Zhi Xiang Lin <zhixiang94@gmail.com>
2015-12-18 17:25:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -r ./vim/.vimrc ~/
+mkdir -p ~/.vim/
 cp -r ./vim/.vim/ ~/.vim/
-cp -r ./git/.gitconfig ~/.git/
+cp -r ./git/.gitconfig ~/
 cat ./bash/.bash_profile >> ~/.bash_profile
