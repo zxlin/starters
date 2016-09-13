73cb668ed372c27b60db150c7df9587a485f36a6
Zhi Xiang Lin <zhixiang94@gmail.com>
2016-09-12 20:40:40 -0400

    Add zsh to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ cp -r ./vim/.vim/* ~/.vim/
 cp -r ./git/.gitconfig ~/
 cp -r ./tmux/.tmux.conf ~/
 cat ./bash/.bash_profile >> ~/.bash_profile
+cp ./zsh/.zshrc ~/.zshrc
+wget https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme -O ~/.oh-my-zsh/themes/honukai.zsh-theme
