f14aa2fa8656db65c389a202dc956dfb516a8aee
Zhi Xiang Lin <zhixiang94@gmail.com>
2017-02-12 21:51:45 -0500

    Add oh-my-zsh setup

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ cp -r ./vim/.vim/* ~/.vim/
 cp -r ./git/.gitconfig ~/
 cp -r ./tmux/.tmux.conf ~/
 cat ./bash/.bash_profile >> ~/.bash_profile
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ./zsh/.zshrc ~/.zshrc
 wget https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm/master/honukai.zsh-theme -O ~/.oh-my-zsh/themes/honukai.zsh-theme
