719b635d2ac4987e1f9a5cdabecdc025f0c07fa1
Toby Lin <zhixiang94@gmail.com>
2020-09-09 17:06:44 -0400

    Update for python3

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 # check for zsh existence
 command -v zsh >/dev/null 2>&1 || { echo >&2 "$1 aborted, please install zsh first"; exit 1; }
-command -v pip >/dev/null 2>&1 || { echo >&2 "$1 aborted, please install python-pip first"; exit 1; }
+command -v pip3 >/dev/null 2>&1 || { echo >&2 "$1 aborted, please install python3-pip first"; exit 1; }
 
-sudo pip install git+git://github.com/Lokaltog/powerline
+sudo pip3 install git+git://github.com/Lokaltog/powerline
 
 git clone https://github.com/VundleVim/Vundle.vim.git ./vim/.vim/bundle/Vundle.vim
 cp -r ./vim/.vimrc ~/
